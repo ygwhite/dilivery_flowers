abb3cc29f564f4cc50af47e5c90c72d3e0f3a184
ygwhite <ru516@yandex.ru>
2023-04-13 00:38:17 +0300

    Запили регу, именно форму, без sql

--- login.py
+++ login.py
@@ -1,27 +1,31 @@
-from kivy.lang.builder import Builder
+from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
 
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
 
-class LoginMenuScreen(Screen):
+
+# Declare both screens
+
+# Builder.load_string(r'E:\Программирование\Проекты\Python\PycharmProjects\kivi\login.kv')
+
+class LoginScreen(Screen):
     pass
-
 
 class RegisterMenuScreen(Screen):
     pass
 
-
 class LoginApp(MDApp):
+
     def build(self):
+        # Create the screen manager
         sm = ScreenManager()
-        sm.add_widget(LoginMenuScreen(name='login_menu'))
+        sm.add_widget(LoginScreen(name='menu'))
         sm.add_widget(RegisterMenuScreen(name='register_menu'))
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
-        # return Builder.load_file('login.kv')
+        return sm
 
-    def logger(self):
-        print(f'sup {self.root.ids.user.text}!')
-
-
-LoginApp().run()
+if __name__ == '__main__':
+    LoginApp().run()
