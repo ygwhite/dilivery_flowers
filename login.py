5121c3ae08c910c357b233cfc20309204cc6a81b
ygwhite <ru516@yandex.ru>
2023-04-12 19:24:59 +0300

    Пилю регу

--- login.py
+++ login.py
@@ -1,9 +1,21 @@
 from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
+
+
+class LoginMenuScreen(Screen):
+    pass
+
+
+class RegisterMenuScreen(Screen):
+    pass
 
 
 class LoginApp(MDApp):
     def build(self):
+        sm = ScreenManager()
+        sm.add_widget(LoginMenuScreen(name='login_menu'))
+        sm.add_widget(RegisterMenuScreen(name='register_menu'))
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
         # return Builder.load_file('login.kv')
